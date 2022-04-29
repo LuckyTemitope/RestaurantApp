54a907c94ae9dfc888f1673807014ecf081c73df
LuckyTemitope <65639852+LuckyTemitope@users.noreply.github.com>
2022-04-29 00:15:31 -0400

    Prepared the app for Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBICG6c_5Dlp67odFRDMiMhIYYGep99-6M",
+    authDomain: "restaurant-record-app.firebaseapp.com",
+    projectId: "restaurant-record-app",
+    storageBucket: "restaurant-record-app.appspot.com",
+    messagingSenderId: "32404907635",
+    appId: "1:32404907635:web:fb7d90e6ca5420ff96ec06"
+  }
 };
